a11ac94b74af1dd65a0118ffedeb7822ff8ef798
R-J <kgerbil@gmail.com>
2021-10-31 10:33:03 -0700

    VSCode works workspaces

--- extension/src/handlers/asbplayer/AsbplayerToVideoCommandForwardingHandler.js
+++ extension/src/handlers/asbplayer/AsbplayerToVideoCommandForwardingHandler.js
@@ -1,21 +1,29 @@
-export default class AsbplayerToVideoCommandForwardingHandler {
-
-    constructor() {
+"use strict";
+exports.__esModule = true;
+var AsbplayerToVideoCommandForwardingHandler = /** @class */ (function () {
+    function AsbplayerToVideoCommandForwardingHandler() {
     }
-
-    get sender() {
-        return 'asbplayer';
-    }
-
-    get command() {
-        return null;
-    }
-
-    handle(request, sender) {
+    Object.defineProperty(AsbplayerToVideoCommandForwardingHandler.prototype, "sender", {
+        get: function () {
+            return 'asbplayer';
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(AsbplayerToVideoCommandForwardingHandler.prototype, "command", {
+        get: function () {
+            return null;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AsbplayerToVideoCommandForwardingHandler.prototype.handle = function (request, sender) {
         chrome.tabs.sendMessage(request.tabId, {
             sender: 'asbplayer-extension-to-video',
             message: request.message,
             src: request.src
         });
-    }
-}
+    };
+    return AsbplayerToVideoCommandForwardingHandler;
+}());
+exports["default"] = AsbplayerToVideoCommandForwardingHandler;
